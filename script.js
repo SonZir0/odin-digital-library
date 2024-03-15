6b95bdc4c535abe8ee8e04bf5dc10410b72f5b61
SonZir0 <129615142+SonZir0@users.noreply.github.com>
2024-03-15 17:42:22 +0400

    Refactor the rest into the UserInterface class

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ const Library = new class {
         }
     }
 
+    get nextBookID() {
+        return this.#nextBookID;
+    }
+
     addNewBook(userInput) {
         let newBook = new this.#Book(...userInput);
         this.#booksArr.push(newBook);
@@ -24,99 +28,96 @@ const Library = new class {
         else console.log("No book with such ID!");
     }
 }
-/*
-Refactor as separate UserInterface class
 
-function addBookToLibrary(userInput) {
-    let newBook = new Book(...userInput, nextBookID);
-    Library.push(newBook);
+const UserInterface = new class {
+    dialog = document.querySelector("dialog");
+    addBookBtn = document.querySelector("dialog+button");
+    cancelBtn = document.querySelector(".close");
+    submitBtn = document.querySelector(".submit");
+    inputsList = document.querySelectorAll("dialog div input");
+    bookCards = document.querySelector(".bookCards");
 
-    // creation of book card on a page
-    let newBookCard = document.createElement("div");
-    newBookCard.classList.add("card");
-    newBookCard.setAttribute("bookID", nextBookID);
+    constructor() {
+        this.addBookBtn.addEventListener("click", () => {
+            this.dialog.showModal();
+        })
 
-    let title = document.createElement("p");
-    title.classList.add("title");
-    title.textContent = '"' + userInput[0] + '"';
+        this.cancelBtn.addEventListener("click", () => {
+            this.dialog.close();
+            this.clearInput()
+        })
 
-    let author = document.createElement("p");
-    author.classList.add("author");
-    author.textContent = "by " + userInput[1];
-
-    let pageCount = document.createElement("p");
-    pageCount.classList.add("pages");
-    pageCount.textContent = userInput[2] + " pages";
-
-    let isFinished = document.createElement("button");
-    isFinished.classList.add("status");
-    if (userInput[3]) {
-        isFinished.textContent = "Finished";
-        isFinished.classList.add("finished");
-    } else isFinished.textContent = "Not read";
-    // isFinished toggle
-    isFinished.addEventListener("click", () => {
-        let result = isFinished.classList.toggle("finished");
-        isFinished.textContent = result ? "Finished" : "Not read";
-    });
-
-    let deleteBtn = document.createElement("button");
-    deleteBtn.classList.add("remove");
-    deleteBtn.textContent = "Delete";
-    deleteBtn.addEventListener("click", (e) => {
-        //remove from Library object
-        let cardID = e.target.parentNode.parentNode.getAttribute("bookID");
-        let indexInLibrary = Library.findIndex((book) => book.ID === +cardID);
-        Library.splice(indexInLibrary, 1);
-        //remove from DOM
-        e.target.parentNode.parentNode.remove();
-    });
-
-    let btnPanel = document.createElement("div");
-    btnPanel.classList.add("buttonsPanel");
-    btnPanel.append(isFinished, deleteBtn);
-
-    newBookCard.append(title, author, pageCount, btnPanel);
-    bookCards.append(newBookCard);
-    nextBookID++;
-};
-
-const bookCards = document.querySelector(".bookCards");
-const dialog = document.querySelector("dialog");
-const addBookBtn = document.querySelector("dialog+button");
-const cancelBtn = document.querySelector(".close");
-const submitBtn = document.querySelector(".submit");
-const inputsList = document.querySelectorAll("dialog div input");
-
-addBookBtn.addEventListener("click", () => {
-    dialog.showModal();
-});
-
-cancelBtn.addEventListener("click", () => {
-    dialog.close();
-    clearInput()
-});
-
-submitBtn.addEventListener("click", (e) => {
-    let inputArray = Array.from(inputsList);
-    let validityArr = inputArray.map((userInput) => userInput.validity.valid);
-
-    if (!validityArr.includes(false)) {
-        let inputValues = inputArray.map((userInput) => userInput.value);
-        // input[3](checkbox) value is "on" instead of true/false, so we take it separately
-        inputValues[3] = inputArray[3].checked;
-        addBookToLibrary(inputValues);
-
-        e.preventDefault();
-        dialog.close();
-        clearInput();
+        this.submitBtn.addEventListener("click", (e) => {
+            let inputArray = Array.from(this.inputsList);
+            let validityArr = inputArray.map((inputElem) => inputElem.validity.valid);
+        
+            if (!validityArr.includes(false)) {
+                let inputValues = inputArray.map((inputElem) => inputElem.value);
+                // input[3](checkbox) value is "on" instead of true/false, so we take it separately
+                inputValues[3] = inputArray[3].checked;
+                this.createNewBook(inputValues);
+        
+                e.preventDefault();
+                this.dialog.close();
+                this.clearInput();
+            }
+        })
     }
-});
 
-function clearInput() {
-    for (let i = 0; i < 3; i++) {
-        inputsList[i].value = "";
+    clearInput() {
+        for (let i = 0; i < 3; i++) {
+            this.inputsList[i].value = "";
+        }
+        this.inputsList[3].checked = false;
     }
-    inputsList[3].checked = false;
+
+    // create book and bookCard, display new bookCard
+    createNewBook(inputValues) {
+        
+        let newBookCard = document.createElement("div");
+        newBookCard.classList.add("card");
+        newBookCard.setAttribute("bookID", Library.nextBookID);
+    
+        let title = document.createElement("p");
+        title.classList.add("title");
+        title.textContent = '"' + inputValues[0] + '"';
+    
+        let author = document.createElement("p");
+        author.classList.add("author");
+        author.textContent = "by " + inputValues[1];
+    
+        let pageCount = document.createElement("p");
+        pageCount.classList.add("pages");
+        pageCount.textContent = inputValues[2] + " pages";
+    
+        let isFinished = document.createElement("button");
+        isFinished.classList.add("status");
+        if (inputValues[3]) {
+            isFinished.textContent = "Finished";
+            isFinished.classList.add("finished");
+        } else isFinished.textContent = "Not read";
+        // isFinished toggle
+        isFinished.addEventListener("click", () => {
+            let result = isFinished.classList.toggle("finished");
+            isFinished.textContent = result ? "Finished" : "Not read";
+        });
+    
+        let deleteBtn = document.createElement("button");
+        deleteBtn.classList.add("remove");
+        deleteBtn.textContent = "Delete";
+        deleteBtn.addEventListener("click", (e) => {
+            let cardID = e.target.parentNode.parentNode.getAttribute("bookID");
+            Library.removeBookWithId(cardID);
+            e.target.parentNode.parentNode.remove();
+        });
+    
+        let btnPanel = document.createElement("div");
+        btnPanel.classList.add("buttonsPanel");
+        btnPanel.append(isFinished, deleteBtn);
+    
+        newBookCard.append(title, author, pageCount, btnPanel);
+        this.bookCards.append(newBookCard);
+        // after addNewBook() runs nextBookID increments, so we invoke it at the end
+        Library.addNewBook(inputValues);
+    }
 }
-*/
